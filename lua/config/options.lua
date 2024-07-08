a99023e868b199c75a76cc537830c91df7371cb6
Yoann Ricordel <yoann.ricordel@qarnot-computing.com>
2024-07-08 16:54:47 +0200

    Disable autoformat
    Hate this when working on an existing codebase, will cripple git blame
    usability

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -8,5 +8,8 @@ vim.opt.winbar = "%=%m %f"
 vim.keymap.set("i", "<leader>k", "<ESC>")
 vim.keymap.set("i", "<leader>w", "<ESC>:w<CR>a")
 vim.keymap.set("n", "<leader>w", ":w<CR>")
+-- Don't autoformat files. Auto-formatting is a pain when working on an existing
+-- codebase.
+vim.g.autoformat = false
 
 vim.opt.guicursor = "a:block,a:blinkon0"
