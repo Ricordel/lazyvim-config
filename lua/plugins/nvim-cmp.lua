fdb88dbb893e80f35a86b3133f5eb64e046e735f
Yoann Ricordel <yoann.ricordel@qarnot-computing.com>
2024-07-08 17:00:20 +0200

    Tweak nvim-cmp completion selection options
    - no preselect by default (will see how that works with real LSPs)
    - select + confirm with Tab
    - confirm selection with CR

---
+++ lua/plugins/nvim-cmp.lua
@@ -0,0 +1,22 @@
+return {
+    {
+        "hrsh7th/nvim-cmp",
+
+        ---@param opts cmp.ConfigSchema
+        opts = function(_, opts)
+            local cmp = require("cmp")
+
+            opts.mapping = cmp.mapping.preset.insert({
+                ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+                ['<C-f>'] = cmp.mapping.scroll_docs(4),
+                ['<C-Space>'] = cmp.mapping.complete(),
+                ['<C-e>'] = cmp.mapping.abort(),
+                ['<CR>'] = cmp.mapping.confirm({ select = false }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+                ['<Tab>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+            })
+
+            opts.completion = { completeopt = "menu,menuone,noselect" }
+        end,
+
+    }
+}
