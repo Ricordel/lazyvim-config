3cf69210575a79fc2cdf2a8ef61d152f85dc46bc
Yoann Ricordel <yoann.ricordel@qarnot-computing.com>
2024-07-08 16:55:39 +0200

    Some of my usual keymaps
    Not to hit ESC, to navigate the quickfix list, ...
    
    Also give bach L and H that are hijacked by I don't know what plugin,
    but I do actually use them as they were indened by Bram.

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,24 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+vim.keymap.set("i", "<leader>k", "<ESC>")
+vim.keymap.set("i", "<leader>w", "<ESC>:w<CR>a", { silent = true })
+vim.keymap.set("n", "<leader>w", ":w<CR>", { silent = true })
+
+-- vim.keymap.set("n", "gt", ":BufferLineCycleNext<CR>", { desc = "Next tab", silent = true })
+-- vim.keymap.set("n", "gT", ":BufferLineCyclePrev<CR>", { desc = "Prev tab", silent = true })
+
+vim.keymap.set("n", "<leader>qn", ":cnext<CR>", { desc = "Next quickfix", silent = true })
+vim.keymap.set("n", "<leader>qp", ":cpreviois<CR>", { desc = "Previous quickfix", silent = true })
+vim.keymap.set("n", "<leader>qo", ":copen<CR>", { desc = "Open quickfix", silent = true })
+vim.keymap.set("n", "<leader>qc", ":cclose<CR>", { desc = "Close quickfix", silent = true })
+
+vim.keymap.set("n", "<leader>ln", ":lnext<CR>", { desc = "Next location", silent = true })
+vim.keymap.set("n", "<leader>lp", ":lpreviois<CR>", { desc = "Previous location", silent = true })
+vim.keymap.set("n", "<leader>lo", ":lopen<CR>", { desc = "Open location", silent = true })
+vim.keymap.set("n", "<leader>lc", ":lclose<CR>", { desc = "Close location", silent = true })
+
+-- Gimme back my L and H, you stupid bufferline!
+vim.keymap.del("n", "L")
+vim.keymap.del("n", "H")
--- lua/config/options.lua
+++ lua/config/options.lua
@@ -3,13 +3,11 @@
 -- Add any additional options here
 
 vim.g.mapleader = ","
-vim.opt.winbar = "%=%m %f"
 
-vim.keymap.set("i", "<leader>k", "<ESC>")
-vim.keymap.set("i", "<leader>w", "<ESC>:w<CR>a")
-vim.keymap.set("n", "<leader>w", ":w<CR>")
 -- Don't autoformat files. Auto-formatting is a pain when working on an existing
 -- codebase.
 vim.g.autoformat = false
+
+vim.opt.winbar = "%=%m %f"
 
 vim.opt.guicursor = "a:block,a:blinkon0"
