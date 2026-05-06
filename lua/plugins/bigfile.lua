f32b1be366f1c726d71b503999c34aa0323f4d5a
Yoann Ricordel <yoann.ricordel@qarnot-computing.com>
2026-05-06 14:31:05 +0200

    style: indentation fixes

--- lua/plugins/bigfile.lua
+++ lua/plugins/bigfile.lua
@@ -1,20 +1,19 @@
 return {
-    {
-        "LunarVim/bigfile.nvim",
-        opts = {
-              filesize = 25, -- size of the file in MiB, the plugin round file sizes to the closest MiB
-              pattern = { "*" }, -- autocmd pattern or function see <### Overriding the detection of big files>
-              features = { -- features to disable
-                "indent_blankline",
-                "illuminate",
-                "lsp",
-                "treesitter",
-                "syntax",
-                "matchparen",
-                "vimopts",
-                "filetype",
-            },
-        },
-    }
+  {
+    "LunarVim/bigfile.nvim",
+    opts = {
+      filesize = 25,
+      pattern = { "*" },
+      features = {
+        "indent_blankline",
+        "illuminate",
+        "lsp",
+        "treesitter",
+        "syntax",
+        "matchparen",
+        "vimopts",
+        "filetype",
+      },
+    },
+  },
 }
-
--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,43 +1,57 @@
 return {
-    "nvim-lualine/lualine.nvim",
-    opts = function(_, opts)
-        local roslyn_loading = false
+  "nvim-lualine/lualine.nvim",
+  opts = function(_, opts)
+    local roslyn_loading = false
 
-        vim.api.nvim_create_autocmd("LspAttach", {
-            callback = function(args)
-                local client = vim.lsp.get_client_by_id(args.data.client_id)
-                if client and client.name == "roslyn" then
-                    roslyn_loading = true
-                end
-            end,
-        })
+    vim.api.nvim_create_autocmd("LspAttach", {
+      callback = function(args)
+        local client = vim.lsp.get_client_by_id(args.data.client_id)
+        if client and client.name == "roslyn" then
+          roslyn_loading = true
+        end
+      end,
+    })
 
-        vim.api.nvim_create_autocmd("User", {
-            pattern = "RoslynInitialized",
-            callback = function()
-                roslyn_loading = false
-            end,
-        })
+    vim.api.nvim_create_autocmd("LspDetach", {
+      callback = function(args)
+        local client = vim.lsp.get_client_by_id(args.data.client_id)
+        if client and client.name == "roslyn" then
+          roslyn_loading = false
+        end
+      end,
+    })
 
-        table.insert(opts.sections.lualine_x, 1, {
-            function() return "󰡱 " .. vim.fs.basename(vim.g._roslyn_slnf) end,
-            cond = function() return vim.g._roslyn_slnf ~= nil end,
-            color = function() return { fg = Snacks.util.color("Comment") } end,
-        })
+    vim.api.nvim_create_autocmd("User", {
+      pattern = "RoslynInitialized",
+      callback = function()
+        roslyn_loading = false
+      end,
+    })
 
-        table.insert(opts.sections.lualine_x, 1, {
-            function() return " Roslyn loading…" end,
-            cond = function() return roslyn_loading end,
-            color = function() return { fg = Snacks.util.color("Comment") } end,
-        })
+    table.insert(opts.sections.lualine_x, 1, {
+      function()
+        local s = vim.g._roslyn_slnf
+        return s and ("󰡱 " .. vim.fs.basename(s)) or ""
+      end,
+      cond = function() return vim.g._roslyn_slnf ~= nil end,
+      color = function() return { fg = Snacks.util.color("Comment") } end,
+    })
 
-        -- Keep the generic lsp status for other servers (csharp_ls, etc.)
-        table.insert(opts.sections.lualine_x, 1, {
-            function() return vim.lsp.status() end,
-            cond = function() return vim.lsp.status() ~= "" end,
-            color = function() return { fg = Snacks.util.color("Comment") } end,
-        })
+    table.insert(opts.sections.lualine_x, 1, {
+      function() return " Roslyn loading…" end,
+      cond = function() return roslyn_loading end,
+      color = function() return { fg = Snacks.util.color("Comment") } end,
+    })
 
-        return opts
-    end,
+    table.insert(opts.sections.lualine_x, 1, {
+      function()
+        local s = vim.lsp.status()
+        return s ~= "" and s or ""
+      end,
+      cond = function() return vim.lsp.status() ~= "" end,
+      color = function() return { fg = Snacks.util.color("Comment") } end,
+    })
+
+    return opts
+  end,
 }
