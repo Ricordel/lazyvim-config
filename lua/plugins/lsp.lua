2b5f22015ad5c661d11ff1df658530a77a27c55e
Yoann Ricordel <yoann.ricordel@qarnot-computing.com>
2026-06-24 10:17:01 +0200

    rosyln: make sure to exclude roslyn_ls from LSP
    Otherwise we start roslyn once with LSP and once with the roslyn plugin,
    not good.
    The roslyn plugin is the one that knows how to filter on slnf so we keep
    this one.

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -3,6 +3,9 @@ return {
     "neovim/nvim-lspconfig",
     opts = {
       inlay_hints = { enabled = false },
+      servers = {
+        roslyn_ls = { enabled = false }, -- conflicts with roslyn.nvim
+      },
     },
   },
 }
